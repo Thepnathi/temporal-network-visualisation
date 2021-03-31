b3a94ae01d9a3e022a4e028d8b9a0cac512321d7
Thepnathi <thepnathi@hotmail.com>
2021-03-31 14:36:06 +0100

    Refactoring of the code

---
+++ client/scripts/Data.js
@@ -0,0 +1,20 @@
+const nodes = [
+    {name: 'Bank'},
+    {name: 'Liverpool Street'},
+    {name: 'St. Pauls'},
+    {name: 'Chancery Lane'},
+    {name: 'Random Place'},
+    {name: 'Oxford Circus'},
+    {name: 'Warren Street'},
+    {name: 'Euston'},
+    {name: 'Kings Cross St Pancras'}
+]
+
+const links = [
+    {source: 0, target: 1},
+    {source: 0, target: 2},
+    {source: 0, target: 3},
+    {source: 5, target: 6},
+    {source: 6, target: 7},
+    {source: 7, target: 8}
+]
--- client/scripts/ForceSimulationExample.js
+++ client/scripts/ForceSimulationExample.js
@@ -1,27 +1,13 @@
 // forceLink function pushes linked node apart from one another
+import { nodes, links } from "Data.js"
 
 let width = 1000, height = 600
 
-var nodes = [
-    {name: 'Bank'},
-    {name: 'Liverpool Street'},
-    {name: 'St. Pauls'},
-    {name: 'Chancery Lane'},
-    {name: 'E'},
-    {name: 'Oxford Circus'},
-    {name: 'Warren Street'},
-    {name: 'Euston'},
-    {name: 'Kings Cross St Pancras'}
-   ]
+const compute_node_size = (nodes, width, height) => {
+    return ((width + height) / nodes.length) / nodes.length
+}
 
-var links = [
-    {source: 0, target: 1},
-    {source: 0, target: 2},
-    {source: 0, target: 3},
-    {source: 5, target: 6},
-    {source: 6, target: 7},
-    {source: 7, target: 8}
-]
+console.log(compute_node_size(nodes, width, height))
 
 const drag = simulation => {
     const dragstarted = event => {
@@ -47,27 +33,7 @@ const drag = simulation => {
         .on("end", dragended)
 }
 
-var simulation = d3.forceSimulation(nodes)
-    .force('charge', d3.forceManyBody())
-    .force('overlap', d3.forceCollide())
-    .force('center', d3.forceCenter(width/2, height/2))
-    .force('link', d3.forceLink().links(links).distance(100))
-    .on('tick', this.ticked);
-
-
-function updateLinks() {
-    var u = d3.select('.links') // select DOM element
-        .selectAll('line')
-        .data(links)    // merge the array of data to the DOM element
-
-    u.enter()   // from here we can add missing DOM element to bind with the data
-        .append('line') // add missing line for each data points
-        .merge(u)
-        .attr('x1', d => d.source.x)    // for each data point we draw line on the coordinate from source to target
-        .attr('y1', d => d.source.y)
-        .attr('x2', d => d.target.x)
-        .attr('y2', d => d.target.y)
-}
+d3.selectAll('rect').attr('width', 10)
 
 function updateNodes() {
     u = d3.select('.nodes') 
@@ -79,15 +45,37 @@ function updateNodes() {
         .append('text')
         .text(d => d.name)
         .merge(u)
+        .attr('font-size', 15)
+        .attr('text-anchor', 'middle')
         .attr('x', d => d.x)
         .attr('y', d => d.y)
         .attr('dy', d => 5)
+}
 
+var simulation = d3.forceSimulation(nodes)
+    .force('charge', d3.forceManyBody())
+    .force('overlap', d3.forceCollide())
+    .force('center', d3.forceCenter(width/2, height/2))
+    .force('link', d3.forceLink().links(links).distance(100))
+    .on('tick', this.ticked);
+
+function updateLinks() {
+    var u = d3.select('.links') // select DOM element
+        .selectAll('line')
+        .data(links)    // merge the array of data to the DOM element
+
+    u.enter()   // from here we can add missing DOM element to bind with the data
+        .append('line') // add missing line for each data points
+        .merge(u)
+        .attr('stroke', '#ccc')
+        .attr('stroke-width', 5)
+        .attr('x1', d => d.source.x)    // for each data point we draw line on the coordinate from source to target
+        .attr('y1', d => d.source.y)
+        .attr('x2', d => d.target.x)
+        .attr('y2', d => d.target.y)
 }
 
 function ticked() {
     updateLinks()
     updateNodes()
 }
-
-draw()
