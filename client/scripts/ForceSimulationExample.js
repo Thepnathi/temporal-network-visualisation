991b479ada1779dee6908cb48768a17e269196d1
Thepnathi <thepnathi@hotmail.com>
2021-03-31 20:08:33 +0100

    Remove home.html and refactor

--- client/scripts/ForceSimulationExample.js
+++ client/scripts/ForceSimulationExample.js
@@ -1,5 +1,24 @@
 // forceLink function pushes linked node apart from one another
-import { nodes, links } from "Data.js"
+const nodes = [
+    {name: 'Bank'},
+    {name: 'Liverpool Street'},
+    {name: 'St. Pauls'},
+    {name: 'Chancery Lane'},
+    {name: 'Random Place'},
+    {name: 'Oxford Circus'},
+    {name: 'Warren Street'},
+    {name: 'Euston'},
+    {name: 'Kings Cross St Pancras'}
+]
+
+const links = [
+    {source: 0, target: 1},
+    {source: 0, target: 2},
+    {source: 0, target: 3},
+    {source: 5, target: 6},
+    {source: 6, target: 7},
+    {source: 7, target: 8}
+]
 
 let width = 1000, height = 600
 
