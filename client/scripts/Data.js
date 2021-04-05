fe9ec84e450b4a1fad0a1e143e0e1ef0f6cb6f25
Thepnathi <thepnathi@hotmail.com>
2021-04-05 22:34:13 +0100

    Add new Data

--- client/scripts/Data.js
+++ client/scripts/Data.js
@@ -1,22 +1,20 @@
 let width = 1000, height = 600
 
-// const nodes = [
-//     {name: 'Bank', coordinates: [51.512884, -0.091397]},
-//     {name: 'Liverpool Street', coordinates: [51.517372, -0.083182]},
-//     {name: 'St. Pauls', coordinates: [51.514936, -0.097567]},
-// ]
-
-const nodes = [
+let nodes = [
     {name: 'Bank', coordinates: [200, 50]},
     {name: 'Liverpool Street', coordinates: [100, 100]},
     {name: 'St. Pauls', coordinates: [300, 150]},
+    {name: 'Westferry', coordinates: [350, 200]},
+    {name: 'Canary Wharf', coordinates: [350, 200]},
+    {name: 'heron Quays', coordinates: [350, 200]},
 ]
 
-
-const links = [
-    {source: 0, target: 1, time: "8:00"},
-    {source: 0, target: 2, time: "8:15"},
+let links = [
+    {source: 0, target: 1, weight: "8:00"},
+    {source: 0, target: 2, weight: "9:00"},
+    {source: 3, target: 4, weight: "33"}
 ]
+
 
 function convert(lat, lon){
     let y = ((-1 * lat) + 90) * (height / 180);
@@ -26,15 +24,4 @@ function convert(lat, lon){
 
 let res = convert(51.512884,-0.091397)
 
-let sw = false
-
-// for (let i = 0; i < nodes.length; i++) {
-//     let lat = nodes[i].coordinates[0];
-//     let lon = nodes[i].coordinates[1];
-//     nodes[i].coordinates = convert(lat, lon)
-//     nodes[i].coordinates[0] += 50
-//     nodes[i].coordinates[1] += 50
-// }
-
-console.log(nodes)
 export { nodes, links }
