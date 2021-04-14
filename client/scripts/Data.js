56b8362b8d3f83e41f34a456b20ecc472408d887
Thepnathi <thepnathi@hotmail.com>
2021-04-14 10:50:44 +0100

    Add small change

--- client/scripts/Data.js
+++ client/scripts/Data.js
@@ -4,19 +4,43 @@ let nodes = [
     {name: 'Bank', coordinates: [200, 50]},
     {name: 'Liverpool Street', coordinates: [100, 100]},
     {name: 'St. Pauls', coordinates: [300, 150]},
-]
 
-// Issue with algorithm works with backward start time
+    {name: 'Station A', coordinates: [300, 150]},
+    {name: 'Station B', coordinates: [300, 150]},
+    {name: 'Station C', coordinates: [300, 150]},
+    {name: 'Station D', coordinates: [300, 150]},
+    {name: 'Station E', coordinates: [300, 150]}
+]
 
 let links = [
     {source: 0, target: 1, start: 800, end: 810, color: "red"},
     {source: 1, target: 0, start: 805, end: 809, color: "red"},
     {source: 0, target: 2, start: 805, end: 809, color: "red"},
     {source: 2, target: 0, start: 805, end: 809, color: "red"},
-    // {source: 3, target: 4, start: 810, end: 817, color: "turquoise"},
-    // {source: 4, target: 5, start: 810, end: 820, color: "turquoise"},
-    // {source: 4, target: 5, start: 820, end: 830, color: "turquoise"}  
+
+    {source: 3, target: 4, start: 805, end: 809, color: "red"},
+    {source: 4, target: 5, start: 805, end: 809, color: "red"},
+    {source: 5, target: 6, start: 805, end: 809, color: "red"},
 ]
+//sort links by source, then target
+links.sort(function(a,b) {
+    if (a.source > b.source) {return 1;}
+    else if (a.source < b.source) {return -1;}
+    else {
+        if (a.target > b.target) {return 1;}
+        if (a.target < b.target) {return -1;}
+        else {return 0;}
+    }
+});
+
+for (var i=0; i<links.length; i++) {
+    if (i != 0 &&
+        links[i].source == links[i-1].source &&
+        links[i].target == links[i-1].target) {
+            links[i].linknum = links[i-1].linknum + 1;
+        }
+    else {links[i].linknum = 1;};
+};
 
 
 function convert(lat, lon){
--- client/scripts/TemporalGraphNetwork.js
+++ client/scripts/TemporalGraphNetwork.js
@@ -1,43 +1,12 @@
 import {mouseoverVertex, mouseoutVertex} from './MouseHandler.js';
 import {drag} from './Drag.js';
-// import {nodes, links} from './Data.js';
+import {nodes, links} from './Data.js';
 
-let nodes = [
-    {name: 'Bank', coordinates: [200, 50]},
-    {name: 'Liverpool Street', coordinates: [100, 100]},
-    {name: 'St. Pauls', coordinates: [300, 150]},
-]
-
-let links = [
-    {source: 0, target: 1, start: 800, end: 810, color: "red"},
-    {source: 1, target: 0, start: 805, end: 809, color: "red"},
-    {source: 0, target: 2, start: 805, end: 809, color: "red"},
-    {source: 2, target: 0, start: 805, end: 809, color: "red"},
-]
-//sort links by source, then target
-links.sort(function(a,b) {
-    if (a.source > b.source) {return 1;}
-    else if (a.source < b.source) {return -1;}
-    else {
-        if (a.target > b.target) {return 1;}
-        if (a.target < b.target) {return -1;}
-        else {return 0;}
-    }
-});
-
-for (var i=0; i<links.length; i++) {
-    if (i != 0 &&
-        links[i].source == links[i-1].source &&
-        links[i].target == links[i-1].target) {
-            links[i].linknum = links[i-1].linknum + 1;
-        }
-    else {links[i].linknum = 1;};
-};
 
 console.log(links)
 let width = 1600, height = 800;
 
-export let defaultCircleRadius = 8, defaultFontSize = 15;
+export let defaultCircleRadius = 10, defaultFontSize = 18;
 export let largerCircleRadius = defaultCircleRadius*2, largerFontSize = defaultFontSize*2;
 
 let startTimeRange = 800, endTimeRange = 820
@@ -72,8 +41,8 @@ svg.append("defs").selectAll("marker")
             .attr("viewBox", "0 -5 10 10")
             .attr("refX", 15)
             .attr("refY", 0.5)
-            .attr("markerWidth", 10)
-            .attr("markerHeight", 10)
+            .attr("markerWidth", 7)
+            .attr("markerHeight", 7)
             .attr("orient", "auto")
     .append("path")
         .attr("d", "M0,-5L10,0L0,5");
@@ -82,9 +51,9 @@ svg.append("defs").selectAll("marker")
 const simulation = d3.forceSimulation(nodes)
         .force('link', d3.forceLink()
             .links(links)
-            .distance(d => 100) // Distance between two edges or links
+            .distance(d => 50) // Distance between two edges or links
             .strength(0.1))     
-        .force('charge', d3.forceManyBody().strength(-300)) // strength() attraction (+) or repulsion (-)
+        .force('charge', d3.forceManyBody().strength(-200)) // strength() attraction (+) or repulsion (-)
         .force('overlap', d3.forceCollide()) // prevent vertex overlap one another
         .force('center', d3.forceCenter(width/2, height/2)) // center the graph 
         .on('tick', tick);    // add vertices and edges elements to canvas
@@ -118,14 +87,6 @@ addVertices.append("text") // Append text elem for each data
         .attr("font-size", defaultFontSize)
         .text(d => d.name)
 
-// let addEdgesLabel = svg.selectAll(".edge") 
-//         .append("text")
-//         .attr("fill", "Black")
-//         .attr("font-size", 12)
-//         .attr("font-weight", 100)
-//         .attr("dy", ".30em")
-//         .attr("text-anchor", "middle")
-
 var thing = svg.append("g")
         .attr("id", "thing")
         .style("fill", "navy")
@@ -137,11 +98,6 @@ thing.append("text")
         .attr("startOffset", "50%")	
         .text("Super")
 
-thing.append("use")
-        .attr("xlink:href", "#dayam")
-        .style("stroke", "black")
-        .style("fill", "none");
-
 function tick() {
     // We only want to add the edge within the time range
     addEdges                // We disable the edge if it is not within the time window 
@@ -152,15 +108,6 @@ function tick() {
             return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
         });
 
-    // addEdgesLabel
-    //     .append("textPath")
-    //     .attr("xlink:href", function(d) {
-    //         return "#" + d.source.name + "-" + d.target.name
-    //     })
-    //     .text(function(d) {
-    //         return d.start
-    //     })
-        
     addVertices.attr("transform", d => "translate(" + d.x + "," + d.y + ")");
 }
 
